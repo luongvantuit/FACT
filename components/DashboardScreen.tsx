367dcaaba01faae12bc3481b6a746c25b2d44c1f
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-29 22:38:40 +0700

    test

--- components/DashboardScreen.tsx
+++ components/DashboardScreen.tsx
@@ -1,11 +1,74 @@
 import React, {Component} from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {colors, dimensions} from '../values';
 export default class DashboardScreen extends Component {
   render() {
     return (
       <SafeAreaView>
-        <ScrollView></ScrollView>
+        <ScrollView>
+          <View style={styles.boxItemContainer}>
+            <View style={styles.boxItemTopLeft}>
+              <Text>Test</Text>
+            </View>
+            <View style={styles.boxItemTopRight}>
+              <Text>Test</Text>
+            </View>
+          </View>
+          <View style={styles.boxItemContainer}>
+            <View style={styles.boxItemBottomLeft}>
+              <Text>Test</Text>
+            </View>
+            <View style={styles.boxItemBottomRight}>
+              <Text>Test</Text>
+            </View>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  boxItemContainer: {
+    width: dimensions.widthWindow,
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  boxItemTopRight: {
+    marginTop: 8,
+    marginRight: 8,
+    height: 120,
+    backgroundColor: colors.black,
+    flexGrow: 1,
+    marginLeft: 4,
+    marginBottom: 4,
+  },
+  boxItemTopLeft: {
+    marginTop: 8,
+    marginRight: 4,
+    height: 120,
+    backgroundColor: colors.black,
+    flexGrow: 1,
+    marginLeft: 8,
+    marginBottom: 4,
+  },
+  boxItemBottomRight: {
+    marginTop: 4,
+    marginRight: 8,
+    height: 120,
+    backgroundColor: colors.black,
+    flexGrow: 1,
+    marginLeft: 4,
+    marginBottom: 8,
+  },
+  boxItemBottomLeft: {
+    marginTop: 4,
+    marginRight: 4,
+    height: 120,
+    backgroundColor: colors.black,
+    flexGrow: 1,
+    marginLeft: 8,
+    marginBottom: 8,
+  },
+});
--- styles/box.ts
+++
@@ -1,4 +0,0 @@
-import { StyleSheet } from "react-native";
-import { colors, dimensions } from "../values";
-
-export const box = StyleSheet.create({ })
