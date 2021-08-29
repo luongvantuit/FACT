d8fa2d4bb65b34f9ec246a4e6dcd70cd6954d757
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-29 21:12:21 +0700

    export

---
+++ components/screens/index.js
@@ -0,0 +1,16 @@
+import CheckOPTSignInScreen from "./CheckOPTSignInScreen";
+import CheckOPTSignUpScreen from "./CheckOPTSignUpScreen";
+import DashboardScreen from './DashboardScreen';
+import SetPasswordScreen from './SetPasswordScreen';
+import SignInScreen from './SignInScreen';
+import SignUpScreen from './SignUpScreen';
+import VerifyPasswordScreen from './VerifyPasswordScreen';
+export {
+    CheckOPTSignInScreen,
+    CheckOPTSignUpScreen,
+    DashboardScreen,
+    SetPasswordScreen,
+    SignInScreen,
+    SignUpScreen,
+    VerifyPasswordScreen,
+}
