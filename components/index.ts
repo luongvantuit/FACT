1e8de29300752642f13efd256c37bc95fe30d45b
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-29 21:47:22 +0700

    navigator

---
+++ components/index.ts
@@ -0,0 +1,8 @@
+import DashboardScreen from "./DashboardScreen";
+import SignInScreen from "./SignInScreen";
+import SignUpScreen from "./SignUpScreen";
+export {
+    DashboardScreen,
+    SignInScreen,
+    SignUpScreen,
+}
