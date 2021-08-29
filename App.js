dd4e7d80913b0809dbb3ae48c4b590ea9dd12446
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-29 20:53:43 +0700

    navigator

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { View, Text } from 'react-native'
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <NavigationContainer>
+
+    </NavigationContainer>
   )
 }
