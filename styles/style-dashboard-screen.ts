9023e1029a1c12084f4708f529e9590c6d85a9d3
Lương Văn Tú <luongvantuit@gmail.com>
2021-08-30 13:39:10 +0700

    box

--- styles/style-dashboard-screen.ts
+++ styles/style-dashboard-screen.ts
@@ -4,6 +4,24 @@ import {colors} from '../values';
 const boxItemBorderRadius: number = 6;
 const boxItemHeight: number = 110;
 
+const shawdowBox = {
+  shadowColor: colors.black,
+  shadowOffset: {
+    width: 0,
+    height: 1,
+  },
+  shadowOpacity: 0.3,
+  shadowRadius: 4.65,
+  elevation: 8,
+};
+
+const box = {
+  height: boxItemHeight,
+  backgroundColor: colors.white,
+  flexGrow: 1,
+  borderRadius: boxItemBorderRadius,
+};
+
 export const styleDashboardScreen = StyleSheet.create({
   boxItemContainer: {
     display: 'flex',
@@ -13,73 +31,33 @@ export const styleDashboardScreen = StyleSheet.create({
   boxItemTopRight: {
     marginTop: 8,
     marginRight: 8,
-    height: boxItemHeight,
-    backgroundColor: colors.white,
-    flexGrow: 1,
     marginLeft: 4,
     marginBottom: 4,
-    shadowColor: colors.black,
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
-    borderRadius: boxItemBorderRadius,
+    ...shawdowBox,
+    ...box,
   },
   boxItemTopLeft: {
     marginTop: 8,
     marginRight: 4,
-    height: boxItemHeight,
-    flexGrow: 1,
     marginLeft: 8,
     marginBottom: 4,
-    backgroundColor: colors.white,
-    shadowColor: colors.black,
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
-    borderRadius: boxItemBorderRadius,
+    ...shawdowBox,
+    ...box,
   },
   boxItemBottomRight: {
     marginTop: 4,
     marginRight: 8,
-    height: boxItemHeight,
-    backgroundColor: colors.white,
-    flexGrow: 1,
     marginLeft: 4,
     marginBottom: 8,
-    shadowColor: colors.black,
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
-    borderRadius: boxItemBorderRadius,
+    ...shawdowBox,
+    ...box,
   },
   boxItemBottomLeft: {
     marginTop: 4,
     marginRight: 4,
-    height: boxItemHeight,
-    backgroundColor: colors.white,
-    flexGrow: 1,
     marginLeft: 8,
     marginBottom: 8,
-    shadowColor: colors.black,
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.3,
-    shadowRadius: 4.65,
-    elevation: 8,
-    borderRadius: boxItemBorderRadius,
+    ...shawdowBox,
+    ...box,
   },
 });
