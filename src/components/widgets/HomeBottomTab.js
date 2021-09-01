f795a54f6e31554db94aed04454227dfbf6e641b
Lương Văn Tú <luongvantuit@gmail.com>
2021-09-01 21:25:29 +0700

    tab

--- src/components/widgets/HomeBottomTab.js
+++ src/components/widgets/HomeBottomTab.js
@@ -5,6 +5,7 @@ import AnalyticsScreen from '../screens/AnalyticsScreen';
 import DashboardScreen from '../screens/DashboardScreen';
 import WalletScreen from '../screens/WalletScreen';
 import MaterialCommunityIcons from '../fonts-icon/material-community-icons'
+import Ionicons from '../fonts-icon/Ionicons'
 import styled from 'styled-components';
 const BottomTab = createBottomTabNavigator();
 
@@ -12,10 +13,19 @@ export default function HomeBottomTab() {
   return (
     <BottomTab.Navigator initialRouteName="Dashboard">
       <BottomTab.Screen component={DashboardScreen} name="Dashboard" options={{
-
+        tabBarIcon: props => (
+          <Container>
+            <MaterialCommunityIcons name='view-dashboard' color={'#000'} />
+            <Text style={{
+              color: '#000'
+            }}>Dashboard</Text>
+          </Container>
+        ),
+        tabBarLabelStyle: {
+          display: 'none'
+        }
       }} />
       <BottomTab.Screen component={AnalyticsScreen} name="Analytic" options={{
-        tabBarShowLabel: false,
         tabBarIcon: props => (
           <Container>
             <MaterialCommunityIcons name='google-analytics' color={'#000'} />
@@ -23,9 +33,24 @@ export default function HomeBottomTab() {
               color: '#000'
             }}>Analytics</Text>
           </Container>
-        )
+        ),
+        tabBarLabelStyle: {
+          display: 'none'
+        }
       }} />
-      <BottomTab.Screen component={WalletScreen} name="Wallet" />
+      <BottomTab.Screen component={WalletScreen} name="Wallet" options={{
+        tabBarIcon: props => (
+          <Container>
+            <Ionicons name='wallet' color={'#000'} />
+            <Text style={{
+              color: '#000'
+            }}>Wallet</Text>
+          </Container>
+        ),
+        tabBarLabelStyle: {
+          display: 'none'
+        }
+      }} />
     </BottomTab.Navigator>
   );
 }
