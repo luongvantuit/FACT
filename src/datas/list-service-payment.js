3dfabaca761f91cb8a9961bf7e1d9171e175d305
Lương Văn Tú <luongvantuit@gmail.com>
2021-09-01 21:32:17 +0700

    test-data

---
+++ src/datas/list-service-payment.js
@@ -0,0 +1,26 @@
+export default listServicePayment = [
+    {
+        id: '1',
+        increase: false,
+        name: 'Highlands Coffee',
+        service: true,
+        value: 10,
+        img: require('../assets/highlands-coffee.png'),
+    },
+    {
+        id: '2',
+        increase: true,
+        name: 'Nguyen Duc Huy',
+        service: false,
+        value: 1000,
+        img: require('../assets/leak-nguyen-duc-huy.jpeg'),
+    },
+    {
+        id: '3',
+        increase: false,
+        name: 'KFC',
+        service: true,
+        value: 29,
+        img: require('../assets/kfc.png'),
+    },
+];
