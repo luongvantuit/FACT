765b4231b67dff018e7662b7188492507ba30ea2
luongvantuit <luongvantuit@gmail.com>
2021-09-09 02:42:22 +0700

    card service

--- src/components/screens/CardServiceScreen.tsx
+++ src/components/screens/CardServiceScreen.tsx
@@ -18,182 +18,219 @@ import shadowBox from '../widgets/ShadowBox';
 export default function CardServiceScreen({navigation}: any) {
   const theme: Theme = useTheme();
   return (
-    <ScrollView showsVerticalScrollIndicator={false}>
-      {/**
-       * start header
-       */}
-      <View
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      alwaysBounceVertical={false}>
+      <LinearGradient
+        colors={[Colors.blueDark, Colors.blueLight, Colors.blueLight]}
         style={{
-          paddingTop: Platform.OS === 'android' ? 0 : 54,
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'center',
-          backgroundColor: theme.colors.background,
+          position: 'relative',
+          top: 0,
+          left: 0,
+          right: 0,
         }}>
-        <Pressable
-          onPress={() => {
-            navigation.goBack();
-          }}
-          style={{
-            marginVertical: 16,
-            marginHorizontal: 16 - 9,
-          }}>
-          <EntypoIcons
-            name="chevron-small-left"
-            size={32}
-            color={Colors.text}
-          />
-        </Pressable>
-        <Text
-          style={{
-            flex: 1,
-            color: Colors.text,
-            textAlign: 'center',
-            fontWeight: '900',
-            fontSize: 18,
-          }}>
-          Card service
-        </Text>
-        <Pressable
-          style={{
-            marginVertical: 16,
-            marginHorizontal: 16 - 9,
-          }}>
-          <EntypoIcons
-            name="chevron-small-left"
-            size={32}
-            color={Colors.transparent}
-          />
-        </Pressable>
-      </View>
-      {/**
-       * end header
-       */}
-      <View
-        style={{
-          marginVertical: 32,
-        }}>
+        {/**
+         * start header
+         */}
         <View
           style={{
-            marginHorizontal: 32,
-          }}></View>
-      </View>
-
-      {
-        ///
-      }
-
-      <View
-        style={{
-          paddingTop: 32,
-          paddingBottom: 4,
-          flex: 1,
-          borderTopLeftRadius: 8,
-          borderTopRightRadius: 8,
-          ...shadowBox,
-          shadowColor: Colors.matteBlack,
-          backgroundColor: Colors.white,
-        }}>
-        {listCards.map((item, index) => (
-          <LinearGradient
-            key={index}
-            colors={
-              item.color || [Colors.blueDark, Colors.blueDark, Colors.blueLight]
-            }
+            paddingTop: Platform.OS === 'android' ? 0 : 54,
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
+          <Pressable
+            onPress={() => {
+              navigation.goBack();
+            }}
             style={{
-              marginHorizontal: 32,
-              marginVertical: 4,
-              display: 'flex',
-              flexDirection: 'row',
-              padding: 16,
-              borderRadius: 8,
-              width: Dimensions.get('window').width - 64,
-              ...shadowBox,
-              height: 180,
+              marginVertical: 16,
+              marginHorizontal: 16 - 9,
             }}>
-            <View
+            <EntypoIcons
+              name="chevron-small-left"
+              size={32}
+              color={Colors.white}
+            />
+          </Pressable>
+          <Text
+            style={{
+              flex: 1,
+              color: Colors.white,
+              textAlign: 'center',
+              fontWeight: '900',
+              fontSize: 18,
+            }}>
+            Card service
+          </Text>
+          <Pressable
+            style={{
+              marginVertical: 16,
+              marginHorizontal: 16 - 9,
+            }}>
+            <EntypoIcons
+              name="chevron-small-left"
+              size={32}
+              color={Colors.transparent}
+            />
+          </Pressable>
+        </View>
+        {/**
+         * end header
+         */}
+        <View
+          style={{
+            marginVertical: 60,
+            display: 'flex',
+            flexDirection: 'row',
+            position: 'absolute',
+            top: 32 + (Platform.OS === 'android' ? 0 : 54),
+            left: 0,
+            right: 0,
+            zIndex: 5,
+            height: 112,
+            borderRadius: 8,
+            ...shadowBox,
+            shadowColor: Colors.matteBlack,
+            marginHorizontal: 32,
+            paddingVertical: 16,
+            backgroundColor: Colors.white,
+          }}>
+          <View
+            style={{
+              width: (Dimensions.get('window').width - 64) / 2,
+              borderRightColor: Colors.blueLight,
+              borderRightWidth: 0.5,
+            }}></View>
+          <View
+            style={{
+              width: (Dimensions.get('window').width - 64) / 2,
+              borderLeftColor: Colors.blueLight,
+              borderLeftWidth: 0.5,
+            }}></View>
+        </View>
+
+        {
+          ///
+        }
+
+        <View
+          style={{
+            marginTop: 120,
+            paddingTop: 32,
+            paddingBottom: 4,
+            flex: 1,
+            borderTopLeftRadius: 8,
+            borderTopRightRadius: 8,
+            ...shadowBox,
+            shadowColor: Colors.matteBlack,
+            backgroundColor: Colors.white,
+          }}>
+          {listCards.map((item, index) => (
+            <LinearGradient
+              key={index}
+              colors={
+                item.color || [
+                  Colors.blueDark,
+                  Colors.blueDark,
+                  Colors.blueLight,
+                ]
+              }
               style={{
+                marginHorizontal: 16,
+                marginVertical: 4,
                 display: 'flex',
-                flexGrow: 3,
-                justifyContent: 'space-between',
+                flexDirection: 'row',
+                padding: 16,
+                borderRadius: 8,
+                width: Dimensions.get('window').width - 32,
+                ...shadowBox,
+                height: 180,
               }}>
-              <Text
+              <View
                 style={{
-                  fontWeight: '900',
-                  fontSize: 18,
-                  color: Colors.neutralLightest,
+                  display: 'flex',
+                  flexGrow: 3,
+                  justifyContent: 'space-between',
                 }}>
-                {item.nameBank}
-              </Text>
-              <View>
                 <Text
                   style={{
-                    fontWeight: '800',
-                    fontSize: 16,
+                    fontWeight: '900',
+                    fontSize: 18,
                     color: Colors.neutralLightest,
                   }}>
-                  {item.numberCard}
+                  {item.nameBank}
                 </Text>
-                <View
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                  }}>
+                <View>
                   <Text
                     style={{
-                      fontSize: 14,
-                      fontWeight: '600',
-                      color: Colors.neutralLightest,
-                    }}>
-                    Expiry end
-                  </Text>
-                  <Text
-                    style={{
-                      marginHorizontal: 8,
                       fontWeight: '800',
+                      fontSize: 16,
                       color: Colors.neutralLightest,
                     }}>
-                    {item.expiryEnd}
+                    {item.numberCard}
+                  </Text>
+                  <View
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'row',
+                    }}>
+                    <Text
+                      style={{
+                        fontSize: 14,
+                        fontWeight: '600',
+                        color: Colors.neutralLightest,
+                      }}>
+                      Expiry end
+                    </Text>
+                    <Text
+                      style={{
+                        marginHorizontal: 8,
+                        fontWeight: '800',
+                        color: Colors.neutralLightest,
+                      }}>
+                      {item.expiryEnd}
+                    </Text>
+                  </View>
+                  <Text
+                    style={{
+                      marginTop: 24,
+                      fontWeight: '800',
+                      fontSize: 16,
+                      color: Colors.neutralLightest,
+                    }}>
+                    {item.name}
                   </Text>
                 </View>
+              </View>
+              <View
+                style={{
+                  display: 'flex',
+                  flexGrow: 3,
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                }}>
+                <FontAwesomeIcons
+                  name={
+                    item.service === 'master-card' ? 'cc-mastercard' : 'cc-visa'
+                  }
+                  size={32}
+                  color={Colors.neutralLightest}
+                />
                 <Text
                   style={{
-                    marginTop: 24,
+                    marginVertical: 4,
                     fontWeight: '800',
                     fontSize: 16,
                     color: Colors.neutralLightest,
                   }}>
-                  {item.name}
+                  {item.type}
                 </Text>
               </View>
-            </View>
-            <View
-              style={{
-                display: 'flex',
-                flexGrow: 3,
-                justifyContent: 'flex-end',
-                alignItems: 'center',
-              }}>
-              <FontAwesomeIcons
-                name={
-                  item.service === 'master-card' ? 'cc-mastercard' : 'cc-visa'
-                }
-                size={32}
-                color={Colors.neutralLightest}
-              />
-              <Text
-                style={{
-                  marginVertical: 4,
-                  fontWeight: '800',
-                  fontSize: 16,
-                  color: Colors.neutralLightest,
-                }}>
-                {item.type}
-              </Text>
-            </View>
-          </LinearGradient>
-        ))}
-      </View>
+            </LinearGradient>
+          ))}
+        </View>
+      </LinearGradient>
     </ScrollView>
   );
 }
