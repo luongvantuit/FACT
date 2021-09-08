b117015a768169cca54c70395cad8c6bc14553ca
luongvantuit <luongvantuit@gmail.com>
2021-09-08 22:32:18 +0700

    text

--- src/components/screens/SelectTypePersonalDocumentsScreen.tsx
+++ src/components/screens/SelectTypePersonalDocumentsScreen.tsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  Dimensions,
-  Platform,
-  Pressable,
-  ImageBackground,
-} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Platform, Pressable, ImageBackground} from 'react-native';
 import {check, request, RESULTS} from 'react-native-permissions';
 import {UndrawOptions} from '../../assets';
 import Colors from '../../assets/colors';
-import RadioButtonGroup from '../widgets/RadioButtonGroup';
+import AntDesignIcons from '../fonts-icon/ant-design-icons';
 import shadowBox from '../widgets/ShadowBox';
 
 export default function SelectTypePersonalDocumentsScreen({navigation}: any) {
+  const [accept, setAccept] = useState<boolean>(false);
   return (
     <View
       style={{
+        flex: 1,
         display: 'flex',
         flexDirection: 'column',
       }}>
       <ImageBackground
         source={UndrawOptions}
         style={{
-          height: 280,
+          height: 340,
         }}
-        resizeMode={'stretch'}
       />
       <View style={{marginHorizontal: 32}}>
         <Text
@@ -35,7 +29,7 @@ export default function SelectTypePersonalDocumentsScreen({navigation}: any) {
             marginBottom: 8,
             color: Colors.text,
           }}>
-          Now. Please Select Indentity Papers
+          Now. Please Check Your Citizenship ID
         </Text>
         <Text
           style={{
@@ -46,24 +40,69 @@ export default function SelectTypePersonalDocumentsScreen({navigation}: any) {
           }}>
           Step 2
         </Text>
-        <RadioButtonGroup items={[{label: 'CCCD/CMND'}, {label: 'Passport'}]} />
       </View>
       <View
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: Dimensions.get('window').height,
-          marginTop: 16,
-          borderTopLeftRadius: 8,
-          borderTopRightRadius: 8,
           ...shadowBox,
           shadowColor: Colors.matteBlack,
           backgroundColor: Colors.white,
+          borderTopLeftRadius: 8,
+          borderTopRightRadius: 8,
+          flexGrow: 1,
         }}>
+        <View
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}>
+          <View
+            style={{
+              marginHorizontal: 32,
+              marginTop: 16,
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <Pressable
+              onPress={() => {
+                setAccept(!accept);
+              }}>
+              {accept === true ? (
+                <AntDesignIcons
+                  name={'checksquare'}
+                  color={Colors.green}
+                  size={28}
+                />
+              ) : (
+                <View
+                  style={{
+                    borderRadius: 4,
+                    borderColor: Colors.text,
+                    width: 28,
+                    height: 28,
+                    borderWidth: 2,
+                  }}
+                />
+              )}
+            </Pressable>
+            <Text
+              style={{
+                fontWeight: '900',
+                color: Colors.text,
+                marginLeft: 8,
+                fontSize: 18,
+              }}>
+              Must Have A Valid Citizenship ID Card
+            </Text>
+          </View>
+        </View>
         <Pressable
           style={{
             marginHorizontal: 32,
-            marginVertical: 16,
+            marginBottom: 32,
+            marginTop: 8,
             justifyContent: 'center',
             paddingVertical: 16,
             borderRadius: 8,
