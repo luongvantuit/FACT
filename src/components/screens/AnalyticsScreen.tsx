15244ea22503b2150a043af6d47c2bffdcfe8319
luongvantuit <luongvantuit@gmail.com>
2021-09-09 23:05:23 +0700

    analytics

--- src/components/screens/AnalyticsScreen.tsx
+++ src/components/screens/AnalyticsScreen.tsx
@@ -38,87 +38,173 @@ export default function AnalyticsScreen({navigation}: any) {
             Analytics
           </Text>
         </View>
-        <LineChart
-          data={{
-            labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-            datasets: [
-              {
-                data: [20.0, 84.5, 54.0, 0.0, 18.5, 220],
-              },
-            ],
-          }}
-          width={Dimensions.get('window').width - 16}
-          height={240}
-          chartConfig={{
-            backgroundColor: Colors.blue,
-            backgroundGradientFrom: Colors.blueDark,
-            backgroundGradientTo: Colors.blue,
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            propsForDots: {
-              r: '3',
-              fill: Colors.yellowDark,
-            },
-          }}
-          yAxisSuffix={user.currency === 'USD' ? '$' : 'VNĐ'}
-          bezier
+        <View
           style={{
-            marginHorizontal: 8,
-            marginTop: 16,
-            borderRadius: 8,
-            ...shadowBox,
-            shadowColor: Colors.matteBlack,
-          }}
-        />
-        <Text
-          style={{
-            fontWeight: '900',
-            fontSize: 18,
-            color: Colors.text,
-            textAlign: 'center',
-            marginTop: 16,
-            marginBottom: 8,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            marginVertical: 16,
           }}>
-          Top categories
-        </Text>
+          <LinearGradient
+            colors={[Colors.blueDark, Colors.blueDark, Colors.blue]}
+            style={{
+              borderRadius: 18,
+              height: 36,
+              width: 74,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginHorizontal: 4,
+              // borderWidth: 0.3,
+              // borderColor: Colors.text,
+            }}>
+            <Text
+              style={{
+                color: Colors.neutralLightest,
+                fontWeight: '900',
+              }}>
+              Week
+            </Text>
+          </LinearGradient>
+          <LinearGradient
+            colors={[
+              Colors.transparent,
+              Colors.transparent,
+              Colors.transparent,
+            ]}
+            style={{
+              backgroundColor: Colors.transparent,
+              borderRadius: 18,
+              height: 36,
+              width: 74,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginHorizontal: 4,
+              borderWidth: 1,
+              borderColor: Colors.blueDark,
+            }}>
+            <Text
+              style={{
+                color: Colors.blueDark,
+                fontWeight: '800',
+              }}>
+              Month
+            </Text>
+          </LinearGradient>
+          <LinearGradient
+            colors={[
+              Colors.transparent,
+              Colors.transparent,
+              Colors.transparent,
+            ]}
+            style={{
+              backgroundColor: Colors.transparent,
+              borderRadius: 18,
+              height: 36,
+              width: 74,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginHorizontal: 4,
+              borderWidth: 1,
+              borderColor: Colors.blueDark,
+            }}>
+            <Text
+              style={{
+                color: Colors.blueDark,
+                fontWeight: '800',
+              }}>
+              Year
+            </Text>
+          </LinearGradient>
+        </View>
         <LinearGradient
-          colors={[Colors.blueDark, Colors.blue, Colors.blue]}
+          colors={[Colors.blueDark, Colors.blue, Colors.blueLight]}
           style={{
-            marginTop: 16,
-            marginHorizontal: 8,
-            borderRadius: 8,
+            borderTopLeftRadius: 8,
+            borderTopRightRadius: 8,
+            paddingTop: 64,
+            paddingBottom: 32,
           }}>
-          <PieChart
+          <LineChart
+            data={{
+              labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+              datasets: [
+                {
+                  data: [20.0, 84.5, 54.0, 0.0, 18.5, 220],
+                },
+              ],
+            }}
             width={Dimensions.get('window').width - 16}
             height={240}
-            data={[
-              {
-                name: 'Service',
-                expense: 36.5,
-                color: Colors.blue,
-                legendFontColor: Colors.neutralLightest,
-              },
-              {
-                name: 'Restaurant',
-                expense: 120,
-                color: Colors.blueLight,
-                legendFontColor: Colors.neutralLightest,
-              },
-              {
-                name: 'Shopping',
-                expense: 240.5,
-                color: Colors.blueLighter,
-                legendFontColor: Colors.neutralLightest,
-              },
-            ]}
             chartConfig={{
-              color: () => `${Colors.blue}`,
-              labelColor: () => `${Colors.blue}`,
+              backgroundColor: Colors.blue,
+              backgroundGradientFrom: Colors.blueDark,
+              backgroundGradientTo: Colors.blue,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              propsForDots: {
+                r: '3',
+                fill: Colors.yellowDark,
+              },
             }}
-            accessor={'expense'}
-            backgroundColor={Colors.transparent}
-            paddingLeft={'8'}
+            yAxisSuffix={user.currency === 'USD' ? '$' : 'VNĐ'}
+            bezier
+            style={{
+              marginHorizontal: 8,
+              borderRadius: 8,
+              ...shadowBox,
+              shadowColor: Colors.matteBlack,
+            }}
           />
+          <Text
+            style={{
+              fontWeight: '900',
+              fontSize: 18,
+              color: Colors.neutralLightest,
+              textAlign: 'center',
+              marginTop: 16,
+              marginBottom: 8,
+            }}>
+            Top categories
+          </Text>
+          <LinearGradient
+            colors={[Colors.blueDark, Colors.blue, Colors.blue]}
+            style={{
+              marginTop: 16,
+              marginHorizontal: 8,
+              borderRadius: 8,
+            }}>
+            <PieChart
+              width={Dimensions.get('window').width - 16}
+              height={240}
+              data={[
+                {
+                  name: 'Service',
+                  expense: 36.5,
+                  color: Colors.blue,
+                  legendFontColor: Colors.neutralLightest,
+                },
+                {
+                  name: 'Restaurant',
+                  expense: 120,
+                  color: Colors.blueLight,
+                  legendFontColor: Colors.neutralLightest,
+                },
+                {
+                  name: 'Shopping',
+                  expense: 240.5,
+                  color: Colors.blueLighter,
+                  legendFontColor: Colors.neutralLightest,
+                },
+              ]}
+              chartConfig={{
+                color: () => `${Colors.blue}`,
+                labelColor: () => `${Colors.blue}`,
+              }}
+              accessor={'expense'}
+              backgroundColor={Colors.transparent}
+              paddingLeft={'8'}
+            />
+          </LinearGradient>
         </LinearGradient>
       </ScrollView>
     </View>
