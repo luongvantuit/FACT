e573b4b7983ed67ef186e69c204ed173fea6972f
luongvantuit <luongvantuit@gmail.com>
2021-09-11 02:53:08 +0700

    permission

--- src/components/screens/QRCodeScreen.tsx
+++ src/components/screens/QRCodeScreen.tsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default function QRCodeScreen() {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <QRCodeScanner
+      onRead={e => {
+        console.log(e.data);
+      }}
+    
+    />
   );
 }
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
