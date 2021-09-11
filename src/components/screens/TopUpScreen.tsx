d0cea80de6f04c3688248e1627e7b401aa58ab0d
luongvantuit <luongvantuit@gmail.com>
2021-09-11 12:47:24 +0700

    box

--- src/components/screens/TopUpScreen.tsx
+++ src/components/screens/TopUpScreen.tsx
@@ -7,6 +7,10 @@ import {
   Pressable,
   Image,
   ScrollView,
+  TextInput,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Keyboard,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {IconApp} from '../../assets';
@@ -58,93 +62,191 @@ export default function TopUpScreen({navigation}: any) {
             Top up to your wallet
           </Text>
         </Pressable>
-        <ScrollView
+        <TouchableWithoutFeedback
+          onPress={Keyboard.dismiss}
           style={{
             flex: 1,
-            borderTopLeftRadius: 8,
-            borderTopRightRadius: 8,
-            backgroundColor: Colors.neutralLightest,
           }}>
-          <View
+          <KeyboardAvoidingView
             style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginTop: 8,
+              flex: 1,
             }}>
             <View
               style={{
-                backgroundColor: Colors.blueLight,
-                paddingVertical: 8,
-                paddingHorizontal: 16,
-                borderRadius: 24,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Text
-                style={{
-                  color: Colors.neutralLightest,
-                  fontWeight: '900',
-                  fontSize: 12,
-                }}>
-                Current account
-              </Text>
-            </View>
-          </View>
-          <View
-            style={{
-              margin: 8,
-              borderRadius: 8,
-              backgroundColor: Colors.neutralLightest,
-              ...shadowBox,
-              shadowColor: Colors.matteBlack,
-              height: 200,
-              padding: 16,
-            }}>
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-                backgroundColor: Colors.neutralLighter,
-                paddingVertical: 8,
-                paddingHorizontal: 16,
-                borderRadius: 8,
+                flex: 1,
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+                backgroundColor: Colors.neutralLightest,
               }}>
               <View
                 style={{
-                  width: 36,
-                  height: 36,
-                  borderRadius: 8,
                   justifyContent: 'center',
                   alignItems: 'center',
-                  backgroundColor: Colors.neutralLightest,
+                  marginTop: 8,
                 }}>
-                <Image
-                  source={IconApp}
+                <View
                   style={{
-                    width: 24,
-                    height: 24,
-                  }}
-                />
+                    backgroundColor: Colors.blueLight,
+                    paddingVertical: 8,
+                    paddingHorizontal: 16,
+                    borderRadius: 24,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      color: Colors.neutralLightest,
+                      fontWeight: '900',
+                      fontSize: 12,
+                    }}>
+                    Current account
+                  </Text>
+                </View>
               </View>
               <Text
                 style={{
-                  marginLeft: 8,
-                  color: Colors.text,
-                }}>
-                Wallet balance:
-              </Text>
-              <Text
-                style={{
+                  textAlign: 'center',
+                  marginTop: 8,
                   fontWeight: '900',
-                  color: Colors.text,
-                  fontSize: 16,
+                  fontSize: 18,
                 }}>
-                {`${user.balance}${user.currency === 'USD' ? '$' : 'VNĐ'}`}
+                {user.name}
               </Text>
+              <View
+                style={{
+                  margin: 8,
+                  borderRadius: 8,
+                  backgroundColor: Colors.neutralLightest,
+                  ...shadowBox,
+                  shadowColor: Colors.matteBlack,
+                  padding: 16,
+                }}>
+                <View
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingVertical: 8,
+                    borderRadius: 8,
+                  }}>
+                  <View
+                    style={{
+                      width: 36,
+                      height: 36,
+                      borderRadius: 4,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      backgroundColor: Colors.neutralLightest,
+                      borderColor: Colors.matteBlack,
+                      borderWidth: 0.5,
+                    }}>
+                    <Image
+                      source={IconApp}
+                      style={{
+                        width: 24,
+                        height: 24,
+                      }}
+                    />
+                  </View>
+                  <Text
+                    style={{
+                      marginLeft: 8,
+                      color: Colors.text,
+                      fontWeight: '800',
+                    }}>
+                    Wallet balance:
+                  </Text>
+                  <Text
+                    style={{
+                      fontWeight: '900',
+                      color: Colors.text,
+                      fontSize: 16,
+                    }}>
+                    {`${user.balance}${user.currency === 'USD' ? '$' : 'VNĐ'}`}
+                  </Text>
+                </View>
+                <TextInput
+                  style={{
+                    borderColor: Colors.blueLight,
+                    borderWidth: 1,
+                    marginTop: 16,
+                    paddingVertical: 8,
+                    paddingHorizontal: 16,
+                    borderRadius: 8,
+                    fontWeight: '900',
+                    color: Colors.text,
+                  }}
+                  placeholder={'Amount'}
+                  keyboardType={'numeric'}
+                  maxLength={9}
+                  placeholderTextColor={Colors.neutralLighter}
+                />
+                <View
+                  style={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <Pressable
+                    style={{
+                      paddingVertical: 4,
+                      paddingHorizontal: 16,
+                      borderColor: Colors.neutralLighter,
+                      borderRadius: 8,
+                      borderWidth: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Text
+                      style={{
+                        color: Colors.text,
+                        fontWeight: '900',
+                      }}>
+                      50$
+                    </Text>
+                  </Pressable>
+                  <Pressable
+                    style={{
+                      paddingVertical: 4,
+                      paddingHorizontal: 16,
+                      borderColor: Colors.neutralLighter,
+                      borderRadius: 8,
+                      borderWidth: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Text
+                      style={{
+                        color: Colors.text,
+                        fontWeight: '900',
+                      }}>
+                      200$
+                    </Text>
+                  </Pressable>
+                  <Pressable
+                    style={{
+                      paddingVertical: 4,
+                      paddingHorizontal: 16,
+                      borderColor: Colors.neutralLighter,
+                      borderRadius: 8,
+                      borderWidth: 1,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    <Text
+                      style={{
+                        color: Colors.text,
+                        fontWeight: '900',
+                      }}>
+                      500$
+                    </Text>
+                  </Pressable>
+                </View>
+              </View>
             </View>
-          </View>
-        </ScrollView>
+          </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
       </LinearGradient>
     </View>
   );
